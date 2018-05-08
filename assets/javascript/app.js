be6f121fd5e29fbb941508efad4dee126bb091b9
Ivan Tellez <ivantellezm@gmail.com>
2018-05-07 23:18:02 -0700

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
+
+//
 $(document).ready(function(){ 
 
 	var gifArray = ["cat","bat","dog","fish", "shark", "falcon", "skunk", "goldfish", "chicken", "chinchilla", "parrot", "falcon", "rabbit", "chicken", "chinchilla", "parrot", "falcon", "rabbit"];
 
-	function appendNewButton(newGif){ 
+	function prependNewButton(newGif){ 
         var createButton = $('<button>')
         createButton.attr('type', "button");
         createButton.addClass('gif');
@@ -17,7 +19,7 @@ $(document).ready(function(){
 
 		function renderButtons(){ 
     	for (var i = 0; i < gifArray.length; i++){
-        appendNewButton(gifArray[i])
+        prependNewButton(gifArray[i])
 		}
 	};
 
@@ -28,30 +30,40 @@ $(document).ready(function(){
 
         // we will call query and limit search to 10
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
-        
-        // This can be done in-line or using multy lines as used in class
-        $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+
+       
+        // Can also use .done(function(response) {
+ 
+        $.ajax({url: queryURL, method: 'GET'}).then(function(response) {
 
         	var results = response.data;
-
+                 // console.log(gifs)
         	for (var i = 0; i < results.length; i++) {
         		var gifDiv = $('<div>');
+                 // Creating a paragraph tag with the result item's rating
         		var p = $('<p>');
+                // Creating an image tag
                 var gifImage = $('<img>');
                 var gifStill = response.data[i].images.fixed_height_still.url;
                 var gifAnimate = results[i].images.fixed_height.url
                 var rating = response.data[i].rating;
+                
 
                 gifImage.addClass('gifToggle');
+                // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
                 gifImage.attr('data-still', gifStill);
                 gifImage.attr('data-animate', gifAnimate);
                 gifImage.attr('data-state', 'still');
                 gifImage.attr('src', gifStill);
 
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
                 gifDiv.append(p);
                 gifDiv.append(gifImage);
                 gifDiv.prepend(rating);
-                gifDiv.prepend('Rating: ')	           	
+                gifDiv.prepend('Rating: ')	   
+
+                // Prepending the gifDiv to the "#gifsView" div in the HTML       	
 
             	$('#gifView').prepend(gifDiv);
         	}
@@ -78,19 +90,20 @@ $(document).ready(function(){
 		$('#gif-input').text('');
 		$('#gifView').empty();
 
-		var gifSearch = $('#gif-input').val().trim();
-		gifArray.push(gifSearch);
-		appendNewButton(gifSearch);
+		
+	
 
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+       // Perfoming an AJAX GET request to our queryURL
 
 		$.ajax({
 	            url: queryURL,
 	            method: 'GET'
 	        })
-	        .done(function(response) {
+        // After the data from the AJAX request comes back
+	        .then(function(response) {
 	        	console.log(response);
 
+             // Saving results
 	        	var results = response.data;
 
 	        	for (var i = 0; i < results.length; i++) {
@@ -107,12 +120,12 @@ $(document).ready(function(){
                     gifImage.attr('data-state', 'still');
                     gifImage.attr('src', gifStill);
 
-                    gifDiv.append(gifImage);
+                    gifDiv.prepend(gifImage);
                     gifDiv.append(p);
                     
 
 	            	$('#gifView').prepend(gifDiv);
-	            	gifState ();
+	            	
 
 	            }
 
@@ -125,6 +138,9 @@ $(document).ready(function(){
 
 		return false;
 	});
+
+    //clistener for on click button to show stills
+    // listener for still to animate Gif
 
 	$(document).on('click', '.gif', displayGifInfo);
 	$(document).on('click', '.gifToggle', gifState);
